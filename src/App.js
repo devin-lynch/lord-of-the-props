66b238caeaaf990a6f432514b0cbd8eee4e5caf4
devin-lynch <devin.l.lynch@gmail.com>
2022-09-22 16:47:35 -0500

    Render via mapping

--- src/App.js
+++ src/App.js
@@ -3,27 +3,43 @@ import Movie from './Movie'
 import './App.css'
 import { toHaveFormValues } from '@testing-library/jest-dom/dist/matchers'
 
+
+const oneArrayToRuleThemAll = [
+  {
+    title: "The Fellowship of the Ring",
+    hours: 2,
+    minutes: 58,
+  },
+  {
+    title: "The Two Towers",
+    hours: 2,
+    minutes: 59,
+  },
+  {
+    title: "The Return of the King",
+    hours: 3,
+    minutes: 21,
+  }
+]
+
 class App extends Component {
   render() {
+    const movies = oneArrayToRuleThemAll.map((movie, i) => {
+      return (
+        <Movie
+          title={movie.title}
+          hours={movie.hours}
+          minutes={movie.minutes}
+          key={`movie-${i}`}
+        />
+      )
+    })
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Lord of the Props</h1>
 
-          <Movie
-          title="The Fellowship of the Ring"
-          hours="2"
-          minutes="58"
-          />
-          <Movie
-          title="The Two Towers"
-          hours="2"
-          minutes="59"
-          />
-          <Movie
-          title="The Return of the King"
-          hours="3"
-          minutes="21"
-          />
+          {movies}
         </header>
       </div>
     )
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -5,7 +5,7 @@ class Movie extends Component {
     render() {
         return (
             <div>
-                <h1>The Lord of the Rings: {this.props.title}</h1>
+                <h2>The Lord of the Rings: {this.props.title}</h2>
                 <p>{this.props.hours}h {this.props.minutes}min</p>
             </div>
         )
